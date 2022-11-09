27d739fd94cc1ed0b2c13ca58c997769af252eb3
AshishKohad27 <kohadashish27@gmail.com>
2022-11-09 21:37:12 +0530

    Added: Breakfast Functionality

---
+++ fitness_club/src/Pages/AshishPages/AddExercise.jsx
@@ -0,0 +1,9 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+export default function AddExercise() {
+    return (
+        <Box>
+           <Heading>Add Exercise Page</Heading>
+        </Box>
+    )
+}
