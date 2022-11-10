6063db21aaedc3216eeb85ef94452527bad615f1
Shubham18598 <muddamwarshubham98@gmail.com>
2022-11-10 23:48:13 +0530

    conflict change

--- fitness_club/src/Redux/AshsihRedux/store.js
+++ fitness_club/src/Redux/AshsihRedux/store.js
@@ -14,6 +14,7 @@ const rootReducer = combineReducers({
 
     AuthReducer:AuthReducer,
     Reducer:Reducer
+    //  hello i am conflict 
 });
 
 const createComposer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
