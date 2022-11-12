7c97b819ff2e6d0a7f6e326184bead9f2a9a7aea
Shubham18598 <muddamwarshubham98@gmail.com>
2022-11-12 11:16:07 +0530

    changes

--- fitness_club/src/index.js
+++ fitness_club/src/index.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
-import { store } from "../src/fitness_clob/store";
-import { Provider } from "react-redux";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-=======
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -32,7 +16,6 @@ root.render(
     <App />
     </Provider>
     </ChakraProvider>
->>>>>>> 17a9de3f82d83f921ae3d1887bfc8acf687b2039
   </BrowserRouter>
 );
 
